a907e800748e002abeb68206cfbcd4bd5525b132
Asm Sayem <Asm Sayem@DESKTOP-5C486UG>
2025-07-15 13:22:55 +0600

    fix: tender guideline crud done

--- src/api/tender-guideline.api.ts
+++ src/api/tender-guideline.api.ts
@@ -1,5 +1,4 @@
 import { api_instance } from "@/apiIntelesence";
-import type { TENDER_GUIDELINE } from "@/types/tender-guideline.type";
 
 export const getAllTenderGuidelines = () =>
   api_instance.get(`Tender/GetAllTenderGuidelines`);
@@ -7,16 +6,11 @@ export const getAllTenderGuidelines = () =>
 export const getTenderGuideline = (id: any) =>
   api_instance.get(`Tender/GetTenderGuideline/${id}`);
 
-export const createTenderGuideline = (data: TENDER_GUIDELINE) =>
+export const createTenderGuideline = (data: any) =>
   api_instance.post(`Tender/CreateTenderGuideline`, data);
 
-export const updateTenderGuideline = ({
-  id,
-  data,
-}: {
-  id: any;
-  data: TENDER_GUIDELINE;
-}) => api_instance.put(`Tender/EditTenderGuideline/${id}`, data);
+export const updateTenderGuideline = (data: any) =>
+  api_instance.put(`Tender/EditTenderGuideline/${data.id}`, data.data);
 
-export const deleteTenderGuideline = (id: number) =>
+export const deleteTenderGuideline = (id: any) =>
   api_instance.delete(`Tender/DeleteTenderGuideline/${id}`);
